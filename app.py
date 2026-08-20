5d9aaccbd5679068e42521d06e838d3ce9043f55
kumarsharath9948-nayakanty <kumarsharath9948@gmail.com>
2026-08-20 13:18:29 +0530

    Revert "include add function and refactor code"

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-def add(x, y):
-    return x + y
-
 def sub(x,y):
     return x-y
 
+print(sub(10,2))
 def multiply(x, y):
     return x * y
 
 print(add(6,3))
-print(sub(10,2))
 print(multiply(18, 13))
